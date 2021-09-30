8cc874d1efd2c823c9510c19642c2e5fd79afe25
askonev <askonev007@gmail.com>
2021-09-30 22:05:55 +0300

    Add helloword plugin

--- start.sh
+++ start.sh
@@ -3,7 +3,7 @@
 source lib/stopwatcher.sh
 source lib/plugin_starter_helper.sh
 
-PLUGIN_NAME=
+PLUGIN_NAME=helloworld
 
 DOCSERVER_DIR_FOR_PLUGINS=/var/www/onlyoffice/documentserver/sdkjs-plugins/plugins
 HOST_IP=$(get_host_ip)
