3a35995ed0a63ad9918a636ba21f8c80c4fa5e0e
maks_sorokin <askonev007@gmail.com>
2022-03-28 14:24:27 +0300

    minor refactor

--- check_plugin_dir.sh
+++ check_plugin_dir.sh
@@ -3,4 +3,4 @@
 PLUGIN_PATH=helloworld/scripts/helloworld.js
 
 docker exec starter_sdkjsplugins_docserver_1 \
-      cat /var/www/onlyoffice/documentserver/sdkjs-plugins/plugins/$PLUGIN_PATH
+      cat /var/www/onlyoffice/documentserver/sdkjs-plugins/$PLUGIN_PATH
