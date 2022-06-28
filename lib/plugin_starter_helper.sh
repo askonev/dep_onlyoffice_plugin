80ef564bb32e05bbd02f0aafe1d8347b5744dd3f
MaxSorokin <askonev007@gmail.com>
2022-06-28 18:04:15 +0300

    fix rule bash analizator

--- lib/plugin_starter_helper.sh
+++ lib/plugin_starter_helper.sh
@@ -5,7 +5,7 @@ function remove_gz_in_dir {
   if [ $? -eq 2 ]; then
       tput setaf 3; echo  "*.gz deleted"; printf '\e[m'
   else
-      for file in $(find ./plugins_list/$PLUGIN_NAME -name '*.gz');
+      for file in $(find ./plugins_list/"$PLUGIN_NAME" -name '*.gz');
         do
           rm $file
         done
