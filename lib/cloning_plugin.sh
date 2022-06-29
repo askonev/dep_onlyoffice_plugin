939b4a17cf3d60306228ccbe07dd321329385001
MaxSorokin <askonev007@gmail.com>
2022-06-29 17:46:09 +0300

    Added method cloning_plugin for clone into remote repo

---
+++ lib/cloning_plugin.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+PLUGIN_NAME="helloworld"
+DS_PLUGINS_DIR="../plugins_list"
+
+function cloning_plugin {
+
+PLUGIN_NAME=$1
+DS_PLUGINS_DIR=$2
+
+REMOTE_REPO="sdkjs-plugins"
+TMP_PLUGINS_DIR=/tmp/$REMOTE_REPO
+
+git clone -b master --depth 1 https://github.com/ONLYOFFICE/"$REMOTE_REPO"/ "$TMP_PLUGINS_DIR"
+
+cp -r --verbose "$TMP_PLUGINS_DIR"/"$PLUGIN_NAME" "$DS_PLUGINS_DIR"
+
+rm -rf "$TMP_PLUGINS_DIR"
+
+}
+
+cloning_plugin "$PLUGIN_NAME" "$DS_PLUGINS_DIR"
