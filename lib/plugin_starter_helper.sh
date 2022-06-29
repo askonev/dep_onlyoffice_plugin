8b39e5a2e0b04a90b01eba7cf1edc57592d2d6db
MaxSorokin <askonev007@gmail.com>
2022-06-29 18:48:21 +0300

    Bash shellcheck disable=SC2044

--- lib/plugin_starter_helper.sh
+++ lib/plugin_starter_helper.sh
@@ -5,6 +5,7 @@ function remove_gz_in_dir {
   if [ $? -eq 2 ]; then
       tput setaf 3; echo  "*.gz deleted"; printf '\e[m'
   else
+      # shellcheck disable=SC2044
       for file in $(find ./plugins_list/"$PLUGIN_NAME" -name '*.gz');
         do
           rm $file
