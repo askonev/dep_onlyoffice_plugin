680b4c783a56e729eb3c12132ea82dd97f7ba9b1
MaxSorokin <askonev007@gmail.com>
2022-07-08 12:41:06 +0300

    Remove check_plugin_dir script

--- check_plugin_dir.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-PLUGIN_PATH=helloworld/scripts/helloworld.js
-
-docker exec starter_sdkjsplugins_docserver_1 \
-      cat /var/www/onlyoffice/documentserver/sdkjs-plugins/$PLUGIN_PATH
