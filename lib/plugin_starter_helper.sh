4e9fb49d7b42349d66b56f10e5f492b4ffd2875c
MaxSorokin <askonev007@gmail.com>
2022-10-03 18:14:35 +0400

    feature/added_grep_172_network

--- lib/plugin_starter_helper.sh
+++ lib/plugin_starter_helper.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 function remove_gz_in_dir {
-
   if [ $? -eq 2 ]; then
       tput setaf 3; echo  "*.gz deleted"; printf '\e[m'
   else
@@ -12,18 +11,14 @@ function remove_gz_in_dir {
         done
       tput setaf 2; echo "*.gz deleted"; printf '\e[m'
   fi
-
 }
 
 function start_document_server_example {
-
     docker-compose exec docserver sudo supervisorctl start ds:example
     docker-compose exec docserver sudo sed 's,autostart=false,autostart=true,' -i /etc/supervisor/conf.d/ds-example.conf
-
 }
 
 function success_check {
-
   # shellcheck disable=SC2181
   if [[ $? -eq 0 ]]
   then
@@ -32,24 +27,36 @@ function success_check {
     tput setaf 1; echo  "Execution error. Exit 1"; printf '\e[m' # red colorized
     exit 1
   fi
-
 }
 
-function get_host_ip {
-
-  ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | \
-        grep -Eo '([0-9]*\.){3}[0-9]*' | \
-        grep -v '127.0.0.1' | \
-        grep -Eo '192.([0-9]*\.){2}([0-9]{1,3})'
-
+function _grep_ip {
+# Searches ip over network 192.*.*.*
+HOST_IP=$(ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | \
+          grep -Eo '([0-9]*\.){3}[0-9]*' | \
+          grep -v '127.0.0.1' | \
+          grep -Eo '192.([0-9]*\.){2}([0-9]{1,3})')
+if [ $HOST_IP ]
+then
+  tput setaf 2; echo  "IP $HOST_IP grepped"; printf '\e[m' # green colorized
+else
+  # Searches ip from the network 172.*.*.* and by wlp3s0
+  HOST_IP=$(ifconfig | grep -A 1 wlp3s0 | \
+                      grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | \
+                      grep -Eo '172.([0-9]*\.){2}([0-9]{1,3})')
+  if [ $HOST_IP ]
+  then
+    tput setaf 2; echo  "IP $HOST_IP grepped"; printf '\e[m' # green colorized
+  else
+    tput setaf 1; echo  "Error searching for IP. Exit 1"; printf '\e[m' # red colorized
+    exit 1
+  fi
+fi
 }
 
-function log_cleaner {
-
+function _log_cleaner {
   LOG_FOLDER="$(pwd)/documentserver_log"
-  sudo rm -r "$LOG_FOLDER/data/*" \
-             "$LOG_FOLDER/db/*" \
-             "$LOG_FOLDER/lib/*" \
-             "$LOG_FOLDER/logs/*"
-
+  sudo rm -r $LOG_FOLDER/data/* \
+             $LOG_FOLDER/db/* \
+             $LOG_FOLDER/lib/* \
+             $LOG_FOLDER/logs/*
 }
